1e3b673e53d2b5b347a65173b005487393f86f5d
Ahmed Missouri <ahmedmissouri@icloud.com>
2023-06-03 07:10:17 +0100

    Implicit and Explicit conversion

---
+++ 9_Implicit_and_Explicit_Conversion/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _9_Implicit_and_Explicit_Conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // implicit conversion
+            int num = 12390532;
+            long bigNum = num;
+
+            float myFloat = 13.37f;
+            double myNewDouble = myFloat;
+
+
+            // explicit conversion
+            double myDouble = 13.37;
+            int myInt;
+            // cast double to int
+            myInt = (int)myDouble;
+            Console.WriteLine(myInt);
+
+
+            // typeConversion
+            string myString = myDouble.ToString();
+            myFloat.ToString();
+        }
+    }
+}
