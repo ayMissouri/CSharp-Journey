0fef54e1febb4ab1c00abf8f704208287bf04995
Ahmed Missouri <ahmedmissouri@icloud.com>
2023-06-03 07:25:51 +0100

    Parsing

---
+++ 10_Parsing/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _10_Parsing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "15";
+            string mySecondString = "13";
+            int num1 = Int32.Parse(myString);
+            int num2 = Int32.Parse(mySecondString);
+            int result = num2 + num1;
+
+            Console.WriteLine(result);
+
+            string stringForFloat = "0.85";
+            string stringForInt = "12345";
+            
+            float myfloat = float.Parse(stringForFloat);
+            int myInt = Int32.Parse(stringForInt);
+            Console.WriteLine(myInt + " " + myfloat);
+        }
+    }
+}
